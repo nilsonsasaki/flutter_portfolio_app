54487c417064bcd9fb11c2512e176cbb8e0060cf
nilsonsasaki <nilsonsasaki@gmail.com>
2021-05-23 17:04:42 -0300

    added BottomButton constants

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,3 +2,8 @@ import 'package:flutter/material.dart';
 
 const TextStyle kLateralTabTextStyle =
     TextStyle(fontSize: 18.0, color: Colors.white);
+
+const TextStyle kBottomButtonTextStyle =
+    TextStyle(fontSize: 18.0, color: Colors.white);
+
+const double kBottomButtonBorderRadius = 10.0;
